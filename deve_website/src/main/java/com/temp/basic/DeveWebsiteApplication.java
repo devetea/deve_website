c5f1111dbc1e4baa5b9b9a10f1f4cca2b282c58f
devetea <108074530+devetea@users.noreply.github.com>
2022-06-27 19:28:17 +0900

    1.테스트 커밋

---
+++ deve_website/src/main/java/com/temp/basic/DeveWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.temp.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveWebsiteApplication.class, args);
+	}
+
+}
---
+++ deve_website/src/test/java/com/temp/basic/DeveWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temp.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
